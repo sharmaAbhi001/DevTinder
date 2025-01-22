2af97b12bd1cfef5c98e53bd78bd54cdad36983e
sharmaAbhi001 <insanepanther97@gmail.com>
2025-01-22 13:49:24 +0530

    exploring Route

--- src/app.js
+++ src/app.js
@@ -7,8 +7,22 @@ app.use("/superb", async (req,res) => {
     res.json({hello:"hii"})
 })
 
-app.get("/hello", async (req,res) => {
+app.get("/marvel", async (req,res) => {
     res.send("hello")
-})
+});
+app.post("/marvel", async (req,res) => {
+    // data saving 
+    res.send("New Super hero Added successfully !")
+});
+app.patch("/marvel",async (req , res) => {
+    // patching the data 
+    res.send("Iron man repair successfully!")
+});
+app.delete("/marvel/:name",async (req,res) => {
+    // deleting a super hero 
+    const hero = req.params;
+    res.send(` ${hero.name} deleted from marvel`)
+});
+
 app.listen(PORT,()=> console.log(`server start port ${PORT}`)
 )
